dddf5bb7e640339a47669a11ca2990a77fc7f391
Raul Janari <rdjanari1707@gmail.com>
2022-09-20 08:52:08 +0200

    Added Data

---
+++ ExpensesAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿
+namespace ExpensesAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Section> Sections { get; set; }
+        public DbSet<Purchase> Purchases { get; set; }
+    }
+}
--- ExpensesAPI/Program.cs
+++ ExpensesAPI/Program.cs
@@ -1,7 +1,16 @@
+global using System.Text.Json.Serialization;
+global using System.ComponentModel.DataAnnotations;
+global using Microsoft.EntityFrameworkCore;
+global using ExpensesAPI.Models;
+global using ExpensesAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+    );
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
