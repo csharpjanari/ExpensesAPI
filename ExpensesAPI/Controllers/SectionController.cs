0e7a61d10430780e20c0bbbd21351c57da73102b
Raul Janari <rdjanari1707@gmail.com>
2022-09-20 08:55:25 +0200

    Logger + Data

---
+++ ExpensesAPI/Controllers/SectionController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExpensesAPI.Controllers
+{
+    [Route("api/Section")]
+    [ApiController]
+    public class SectionController : ControllerBase
+    {
+        private readonly ILogger<SectionController> _logger;
+        private readonly ApplicationDbContext _context;
+
+        public SectionController(ILogger<SectionController> logger, ApplicationDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+    }
+}
