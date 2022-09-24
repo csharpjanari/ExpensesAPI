ab2f689b1d52c14af11f91076e064b5927ed8fa1
Raul Janari <rdjanari1707@gmail.com>
2022-09-24 14:10:14 +0200

    Attribute Required

--- ExpensesAPI/Models/Section.cs
+++ ExpensesAPI/Models/Section.cs
@@ -3,6 +3,7 @@
     public class Section
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; } = string.Empty;
         [JsonIgnore]
         public ICollection<Purchase> Purchases { get; set; }    
