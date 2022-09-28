d39c438e8ef7d00d8f4e0c9a3e3b2ea9df01b97d
Raul Janari <rdjanari1707@gmail.com>
2022-09-28 20:01:13 +0200

    New controller, which will show more information!

---
+++ ExpensesAPI/Controllers/MoreInformationController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExpensesAPI.Controllers
+{
+    [Route("api/MoreInformation")]
+    [ApiController]
+    public class MoreInformationController : ControllerBase
+    {
+        private readonly ILogger<MoreInformationController> _logger;
+        private readonly ApplicationDbContext _context;
+
+        public MoreInformationController(ILogger<MoreInformationController> logger, ApplicationDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+
+
+        [HttpGet("AllPurchases")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult GetAll()
+        {
+            if (_context.Purchases == null)
+            {
+                return NotFound("Purchases not created yet!");
+            }
+
+            _logger.LogInformation("GETTING PURCHASES");
+            return Ok(_context.Purchases);
+        }
+
+
+
+        [HttpGet("LastPurchase")]
+        public IActionResult LastPurchase()
+        {
+            if (_context.Purchases == null)
+            {
+                return NotFound("Purchases not created yet!");
+            }
+
+            var lastPurchase = _context.Purchases.OrderBy(x => x.Id)
+                                                 .LastOrDefault();
+            
+
+            _logger.LogInformation("LOADING LAST PURCHASE...");
+            return Ok(lastPurchase);
+        }
+    }
+}
